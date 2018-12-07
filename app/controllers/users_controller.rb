76f62a47c671ee95d978402c564fa2ed15f68564
EC2 Default User <ec2-user@ip-172-31-19-104.ap-southeast-1.compute.internal>
2018-12-07 08:25:22 +0000

    Finish following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   def create
     #@user = User.new(name: params[:name], email: params[:email], password: params[:password])
     @user = User.new(user_params)
+    
     if @user.save
       flash[:notice] = "ユーザー登録が完了しました"
       redirect_to '/'
@@ -30,12 +31,32 @@ class UsersController < ApplicationController
     ##whereメソッドの戻り値は、「relationクラス」のインスタンスの為、「arrayクラス」に変換（ pushメソッドを使用したい）
     ##dupメソッドにてコピーを作成（arrayクラスに変換すると、freeze=trueとなっており、pushメソッドが使用できない）
     @meetings = Meeting.where(userid: params[:id]).to_ary().dup()
- 
-    tmp = Meeting.where(userid: 3).to_ary()
-    #↑相互フォローユーザのMeeting取得時に、本人のMeetingを取得しないようにコントロールが必要
-    tmp.each do |record|
-      @meetings.push(record)
+    
+    relationships1 = Relationship.where(follower_id: session[:user_id])
+    relationships2 = Relationship.where(followed_id: session[:user_id])
+    
+    relationships1.each do |relationship1|
+      relation = relationships2.find_by(follower_id: relationship1.followed_id)
+      
+      if relation 
+        tmp = Meeting.where(userid: relation.follower_id).to_ary()
+        #↑相互フォローユーザのMeeting取得時に、本人のMeetingを取得しないようにコントロールが必要
+        
+        tmp.each do |record|
+        @meetings.push(record)
+        end
+        
+      end
+      
     end
+    
+ 
+    #tmp = Meeting.where(userid: 3).to_ary()
+    #↑相互フォローユーザのMeeting取得時に、本人のMeetingを取得しないようにコントロールが必要
+    #tmp.each do |record|
+      
+      #@meetings.push(record)
+    #end
     
   end
   
