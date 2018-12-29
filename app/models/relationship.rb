eb0d04ac9dcdb5f051c47a9e01adaa2627253a70
EC2 Default User <ec2-user@ip-172-31-19-104.ap-southeast-1.compute.internal>
2018-12-29 08:21:11 +0000

    Finish Edit Models

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -2,6 +2,6 @@ class Relationship < ApplicationRecord
   validates :follower_id, presence: true
   validates :followed_id, presence: true
   
-  belongs_to :user, class_name: 'User'
+  belongs_to :user, class_name: 'User' ,optional: true
   
 end
